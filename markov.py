0fe2301045b182fade6f163157d9f051deb415fd
TYLPHE <tylphet@gmail.com>
2023-04-11 14:03:46 -0600

    Add parameter for n words

--- markov.py
+++ markov.py
@@ -14,7 +14,7 @@ def open_and_read_file(file_path):
     return contents
 
 
-def make_chains(text_string):
+def make_chains(text_string, n_words):
     """Take input text as string; return dictionary of Markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -42,53 +42,65 @@ def make_chains(text_string):
     chains = {}
 
     lst = text_string.split()
-    
-    for i in range(len(lst) - 2):
-        word1 = lst[i]
-        word2 = lst[i + 1]
-        value = chains.get((word1, word2), [])
+
+    for i in range(len(lst) - n_words):
+        tuple_list = []
+        for n in range(n_words):
+            tuple_list += [lst[n + i]]
+
+        value = chains.get(tuple(tuple_list), [])
         if len(value) != 0:
-            value.append(lst[i + 2])
+            value.append(lst[i + n_words])
         else: 
-            chains[(word1, word2)] = [lst[i + 2]]
+            chains[tuple(tuple_list)] = [lst[i + n_words]]
 
-
-    for key in sorted(chains):
-        print(key, chains[key])
+    
+    # for key in sorted(chains):
+    #     print(key, chains[key])
     # print(type(sorted(chains)))
     return chains
 
 
-def make_text(chains):
+def make_text(chains, n_words):
     """Return text from chains."""
+    # for key in sorted(chains):
+    #     print(key, chains[key])
 
     words = []
     lst = list(chains.keys())
     random_key = choice(lst)
     
-    words.append(random_key[0])
-    words.append(random_key[1])
+    for n in range(n_words):
+        words.append(random_key[n])
 
     while chains.get(random_key):
         random_word = choice(chains[random_key]) 
         words.append(random_word)
+        tuple_key = []
 
-        random_key = (random_key[1], random_word)
+        for n in range(1, n_words):
+            tuple_key += [random_key[n]]
 
-    # print(words)
+        tuple_key += [random_word]
+        random_key = tuple(tuple_key)
+
 
     return ' '.join(words)
 
 
-input_path = 'gettysburg.txt'
+input_path = sys.argv[1]
+try:
+    n_words = int(sys.argv[2])
+except IndexError:
+    n_words = 2
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains(input_text, n_words)
 
 # Produce random text
-random_text = make_text(chains)
+random_text = make_text(chains, n_words)
 
 print(random_text)
